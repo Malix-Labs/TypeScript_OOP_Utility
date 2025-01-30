6355a9b5eb5b406d5017bfee46f81f1dcf997d4b
Malix <alixbrunetcontact@gmail.com>
2025-01-30 00:17:06 +0000

    feat: Constructor type

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-export function add(a: number, b: number): number {
-	return a + b;
-}
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-	console.log("Add 2 + 3 =", add(2, 3));
-}
+export type Constructor<Class extends object = object, Arguments extends any[] = any[]> = new (...args: Arguments) => Class;
